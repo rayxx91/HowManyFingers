aa0715949e8e2666d8c24039c93b583204fe302c
Alex Chaleroux <rayxx91@gmail.com>
2020-11-09 13:25:21 +0100

    Initial Commit

---
+++ HowManyFinguers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HowManyFinguers
+//
+//  Created by chaleroux on 09/11/2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
